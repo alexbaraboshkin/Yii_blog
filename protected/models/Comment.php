66c8ea7544017e89d520f1d337dbf5303835a08e
alexbaraboshkin <abaraboshkin@yandex.com>
2015-03-19 18:08:24 +0500

    В модели Comment поправлены методы rules() и attributeLabels(). Переопределен метод beforeSave()

--- protected/models/Comment.php
+++ protected/models/Comment.php
@@ -33,16 +33,12 @@ class Comment extends CActiveRecord
 	 */
 	public function rules()
 	{
-		// NOTE: you should only define rules for those attributes that
-		// will receive user inputs.
-		return array(
-			array('content, status, author, email, post_id', 'required'),
-			array('status, create_time, post_id', 'numerical', 'integerOnly'=>true),
-			array('author, email, url', 'length', 'max'=>128),
-			// The following rule is used by search().
-			// @todo Please remove those attributes that should not be searched.
-			array('id, content, status, create_time, author, email, url, post_id', 'safe', 'on'=>'search'),
-		);
+        return array(
+            array('content, author, email', 'required'),
+            array('author, email, url', 'length', 'max'=>128),
+            array('email','email'),
+            array('url','url'),
+        );
 	}
 
 	/**
@@ -62,16 +58,16 @@ class Comment extends CActiveRecord
 	 */
 	public function attributeLabels()
 	{
-		return array(
-			'id' => 'ID',
-			'content' => 'Content',
-			'status' => 'Status',
-			'create_time' => 'Create Time',
-			'author' => 'Author',
-			'email' => 'Email',
-			'url' => 'Url',
-			'post_id' => 'Post',
-		);
+        return array(
+            'id' => 'Id',
+            'content' => 'Comment',
+            'status' => 'Status',
+            'create_time' => 'Create Time',
+            'author' => 'Name',
+            'email' => 'Email',
+            'url' => 'Website',
+            'post_id' => 'Post',
+        );
 	}
 
 	/**
@@ -116,4 +112,15 @@ class Comment extends CActiveRecord
 	{
 		return parent::model($className);
 	}
+    protected function beforeSave()
+    {
+        if(parent::beforeSave())
+        {
+            if($this->isNewRecord)
+                $this->create_time=time();
+            return true;
+        }
+        else
+            return false;
+    }
 }

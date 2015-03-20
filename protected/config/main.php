74b1ca7dfc729239f8c19aa145d59f3cf59556ce
alexbaraboshkin <abaraboshkin@yandex.com>
2015-03-20 14:19:21 +0500

    Человекопонятные URL в конфигурации приложения в urlmanager'е

--- protected/config/main.php
+++ protected/config/main.php
@@ -56,7 +56,15 @@ return array(
 			),
 		),
 		*/
-
+        'urlManager'=>array(
+            'urlFormat'=>'path',
+            'rules'=>array(
+                'post/<id:\d+>/<title:.*?>'=>'post/view',
+                'posts/<tag:.*?>'=>'post/index',
+                'post/update/<id:\d+>'=>'post/update',
+                '<controller:\w+>/<action:\w+>'=>'<controller>/<action>',
+            ),
+        ),
 		// database settings are configured in database.php
 		'db'=>require(dirname(__FILE__).'/database.php'),
 

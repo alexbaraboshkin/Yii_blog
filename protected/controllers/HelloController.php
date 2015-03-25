0846a2b8697629150d5d785c0f14832c6c238870
alexbaraboshkin <abaraboshkin@yandex.com>
2015-03-25 17:18:45 +0500

    пока новость из файла читать не хочет(

--- protected/controllers/HelloController.php
+++ protected/controllers/HelloController.php
@@ -33,5 +33,13 @@ class HelloController extends CController
     public function actionNews()
     {
         echo "Вы открыли новость: ", $_GET['news_name'];
+        if (!empty($_GET['news_name']))
+        {
+            // если значение не пустое...
+            // выводим содержимое переданного файла с расширением txt
+//            echo file_get_contents($_GET['news_name'] . ".txt");
+//            echo file_get_contents(dir(__FILE__)."/../../public/" . $_GET['news_name'] . ".txt");
+        }
     }
+
 }

fa22dae218d03b961f8651023e8a6f878b3e1114
alexbaraboshkin <abaraboshkin@yandex.com>
2015-03-25 15:45:28 +0500

    добавлено несколько контроллеров и переданы данные get-методом

--- protected/controllers/HelloController.php
+++ protected/controllers/HelloController.php
@@ -12,5 +12,26 @@ class HelloController extends CController
     public function actionWorld()
     {
         echo 'Привет от HelloController->actionWorld()';
+//        echo YiiBase::getPathOfAlias(system.web.CController);
+    }
+    public function actionStepan()
+    {
+        echo 'Привет Степан!';
+    }
+    public function actionDima()
+    {
+        echo 'Привет Дима!';
+    }
+    public function actionIvan()
+    {
+        echo 'Привет Иван!';
+    }
+//    public function actionNews()
+//    {
+//        echo “Вы открыли новость: ” , $_GET['news_name'];
+//    }
+    public function actionNews()
+    {
+        echo "Вы открыли новость: ", $_GET['news_name'];
     }
 }
